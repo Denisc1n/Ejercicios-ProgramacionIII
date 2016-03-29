9b67645dfa3468f55183d1833c2f900e831159ec
Denisc1n <denis.l.pedemonte@gmail.com>
2016-03-29 08:56:25 -0300

    Signed-off-by: Denisc1n <denis.l.pedemonte@gmail.com>

--- ClaseTresProgramacionIII/ElMayor.php
+++ ClaseTresProgramacionIII/ElMayor.php
@@ -8,16 +8,32 @@
 <input type="Text" name="Primero">
 <input type="Text" name="Segundo">
 <input type="Text" name="Tercero">
-<input type="Submit" value="Calcular">
+<input type="Submit" name="Uno"value="Calcular">
+<input type="Submit" name="Dos"value="Hacer">
+<input type="Submit" name="Tres" value="Otro Boton">  
+
 
 
 </form>
 <?php
 
+if (isset($_POST['Uno'])) {
+	echo"<br>"."UNO"."<br>";
+		
+}
+elseif (isset($_POST['Dos'])) {
+	echo "<br>"."DOS"."<br>";
+}
+elseif (isset($_POST['Tres'])) {
+	echo"<br>"."DOS"."<br>";;
+}
+
 var_dump($_POST);
 $max = max($_POST);
 echo "<br>"."<br>".$max;
 ?>
+
+<!--isset() devuelve true si esta seteado o false si es null.  -->
 
 </body>
 </html>
