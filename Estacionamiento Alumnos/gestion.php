66874e59488a0e9bcd2fc5cf9d843ec31459205d
Denisc1n <denis.l.pedemonte@gmail.com>
2016-04-05 08:48:06 -0300

    Se completo la baja de vehiculos.
    Baja de vehiculos con escritura posterior completa.
    Se realizo usando foreach y dos array de autos para dar las bajas.

--- Estacionamiento Alumnos/gestion.php
+++ Estacionamiento Alumnos/gestion.php
@@ -15,13 +15,57 @@
 $accion=$_POST["estacionar"];
 $patente=$_POST["patente"];
 $ahora=date("Y-M-d h:i:s");
-
+$listaDeAutos=array();
+$listaAuxiliar=array();
 
 if ($accion=="ingreso") {
 	echo "Se guardo la patente $patente";
 	$archivo=fopen("Ticket.txt", "a");
 	fwrite($archivo, "$patente"."-----"."$ahora"."\n");
 	fclose($archivo);
+}else{
+	$archivo=fopen("Ticket.txt", "r");
+	while (!feof($archivo)) {
+		$renglon=fgets($archivo);
+		$auto=explode("-----", $renglon);
+		$listaDeAutos[]=$auto;
+	}
+//var_dump($listaDeAutos);
+	fclose($archivo);
+	$esta=false;
+	foreach ($listaDeAutos as $auto) {
+		echo $auto[0]."<br>";
+		if ($auto[0]==$patente) {
+			$esta=true;
+			$fechaInicio=$auto[1];
+			$diferencia=strtotime($ahora)-strtotime($fechaInicio);
+			echo "El tiempo transcurrido es $diferencia";
+			//unset($listaDeAutos[$id]);
+		}
+		else{
+
+			if ($auto[0]!="") {
+				$listaAuxiliar[]=$auto;
+			}
+		}
+
+
+	}	
+
+
+	if ($esta) {
+		echo "El Auto se encuentra estacionado.";
+		fopen("Ticket.txt", "w");
+		foreach ($listaAuxiliar as $auto) {
+			if ($auto[0]!=$patente) {
+				fwrite($archivo, $auto[0]."-----".$auto[1]);
+			}
+		}
+	} else{
+		echo "El Auto no se encuentra estacionado.";
+	}
+
+
 }
 
 //var_dump($_POST["estacionar"]);
